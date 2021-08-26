db9185a3793a266ca97f507794cd09dafd54ab37
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-26 18:53:06 -0400

    removed comments

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,7 +3,6 @@ import BeatBoxLogo from "../components/BeatBoxLogo";
 const Header = ()=> {
   return(
     <div id="header">
-      {/* <p>LOGO HERE</p> */}
       <BeatBoxLogo />
       <h2>Great Music. Unexpected Finds. Tailored For You</h2>
     </div>
