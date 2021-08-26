e7e52e2e8be9c8ad092a32e7a2a7fb111bc05671
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-26 19:48:20 -0400

    added img files

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,14 @@
-import BeatBoxLogo from "../components/BeatBoxLogo";
+// import BeatBoxLogo from "../components/BeatBoxLogo";
+import {ReactComponent as BeatBoxLogo} from '../svgs/beatBox-header-logo.svg';
+import {ReactComponent as HeaderBackground} from '../svgs/header-background.svg';
+import './Header.css'
 
 const Header = ()=> {
+  const headerStyle = {
+    backgroundImage :`url(${HeaderBackground})`
+  }
   return(
-    <div id="header">
+    <div id="header" styles={headerStyle}>
       <BeatBoxLogo />
       <h2>Great Music. Unexpected Finds. Tailored For You</h2>
     </div>
