a6c8e06699cf05b3eca17b13f428cd92c9896a3e
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 03:21:24 -0400

    connected login component to app comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './containers/Header';
 import Navbar from './containers/Navbar';
 import Register from './containers/Register';
+import Login from './containers/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Navbar />
       <Register />
+      <Login />
     </>
   );
 }
