55137e38396e3345fd5cab7929f4affc7d0956a9
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 00:06:56 -0400

    added img

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,9 @@
 import './Home.css'
+import dude from '../assets/Afropunk.png'
 const Home = () => {
   return (
     <div id="home">
+      <img src={dude} alt="some guy chilling" />
 
     </div>
   )
