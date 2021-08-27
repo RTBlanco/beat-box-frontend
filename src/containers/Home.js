d24dd39e21669809d8bad5754f2d11a09ccdf547
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 01:03:33 -0400

    added img

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,8 @@ import dude from '../assets/Afropunk.png'
 const Home = () => {
   return (
     <div id="home">
+      <h2>Looking to start your music exploration journey?</h2>
+      <button className="lime-btn">Get Started</button>
       <img src={dude} alt="some guy chilling" />
 
     </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './assets/NicoMoji-Regular.ttf';
 
 
 ReactDOM.render(
