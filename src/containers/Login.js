4f8278b82f3a89fe7f59140172caf3bb938ccc6b
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 04:10:28 -0400

    added missing button

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,6 +4,10 @@ import boombox from '../assets/boombox.png'
 const Login = () => {
   return (
     <div className="login-area">
+      <div id="log-content">
+        <p>Already Registerd?</p>
+        <button className="dark-btn">Get Premium</button>
+      </div>
       <img id="boombox" src={boombox} alt="boombox" />
     </div>
   )
