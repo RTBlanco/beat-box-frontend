01877625d9d6ff2fff4cca2e28245dbb72b0db13
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-26 22:46:02 -0400

    added home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './containers/Header';
 import Navbar from './containers/Navbar';
+import Home from './containers/Home';
 
 function App() {
   return (
     <>
       <Header />
       <Navbar />
+      <Home />
     </>
   );
 }
