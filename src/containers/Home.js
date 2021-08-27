18b13ff659533c4521e2bbead3322c2a1e0887cf
RTBlanco <ronnytoribio1@hotmail.com>
2021-08-27 01:31:07 -0400

    made it use flex box

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,10 +3,14 @@ import dude from '../assets/Afropunk.png'
 const Home = () => {
   return (
     <div id="home">
-      <h2>Looking to start your music exploration journey?</h2>
-      <button className="lime-btn">Get Started</button>
       <img src={dude} alt="some guy chilling" />
-
+      <div id="content">
+        <h2>Looking to start your music exploration journey?</h2>
+        <button className="lime-btn">Get Started</button>
+      </div>
+      <div id="description">
+        When was the last time you fell in love with a new artist or song? Beat.box helps you find artists you love by creating tailored playlists just for you.
+      </div>
     </div>
   )
 }
